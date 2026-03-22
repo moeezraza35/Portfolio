887d1be7628bc73f7e89a09f27167894f22308a3
Moeez Raza <moeezrazaseven@gmail.com>
2026-03-22 16:04:57 +0500

    Improved Intro Section

--- frontend/src/components/content.tsx
+++ frontend/src/components/content.tsx
@@ -1,5 +1,49 @@
-import type { ReactNode } from "react"
+import { useContext, useEffect, useRef, type ReactNode } from "react"
+import { ScrollContext, type Section } from "../context/scroll"
 
+function SingleColumnContent(props: {children?: ReactNode, name: string}){
+  const {scrollY, setSections} = useContext(ScrollContext)
+  const thisSection = useRef<HTMLElement>(null)
+  useEffect(() => {
+    if (!thisSection.current) return
+    setSections((prev: Section[]) => [
+      ...prev, {
+        name: props.name,
+        start: thisSection.current?.offsetTop,
+        height: thisSection.current?.offsetHeight
+      }
+    ])
+  }, [setSections])
+  useEffect(() => {
+    const viewHeight = window.innerHeight
+    if (!thisSection.current) return
+    const viewStart = thisSection.current.offsetTop
+    const scrollStart = scrollY - viewStart
+    if (
+      scrollStart < 0 ||
+      scrollStart > thisSection.current.clientHeight+viewStart
+    ) return // Optimizing the performance
+
+    const ratio = scrollStart / viewHeight
+    thisSection.current.childNodes.forEach(node => {
+      if (node instanceof HTMLElement){
+        node.style.opacity = "" + (1 - ratio)
+        node.style.scale = "" + (1 + ratio)
+      }
+    })
+  }, [scrollY])
+  return (
+    <section ref={thisSection} className="h-[200vh]">
+      <div className="slider">
+        <div className="slider-text">
+          { props.children }
+        </div>
+        <img src="/images/background-left.png" className="img-left"/>
+        <img src="/images/background-right.png" className="img-right"/>
+      </div>
+    </section>
+  )
+}
 function DoubleSidedContent(props: {title:string, children?:ReactNode, img:string}) {
   return (
     <div className="doublesided-content">
@@ -19,4 +63,4 @@ function CardContent() {
     </div>
   )
 }
-export { DoubleSidedContent, CardContent }
+export { SingleColumnContent, DoubleSidedContent, CardContent }
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,56 +1,19 @@
-import { useContext, useEffect, useRef } from "react"
-import { ScrollContext, type Section } from "../context/scroll"
-import { DoubleSidedContent } from "../components/content"
+import { DoubleSidedContent, SingleColumnContent } from "../components/content"
 
-function HeroSection(){
-  const {scrollY, setSections} = useContext(ScrollContext)
-  const heroSection = useRef<HTMLElement>(null)
-  useEffect(() => {
-    if (!heroSection.current) return
-    setSections((prev: Section[]) => [
-      ...prev, {
-        name: "home", 
-        start: heroSection.current?.offsetTop, 
-        height: heroSection.current?.offsetHeight
-      }
-    ])
-  }, [setSections])
-  useEffect(() => {
-    const viewHeight = window.innerHeight
-    // if (scrollY > viewHeight+1) return
-    if (!heroSection.current) return
-    heroSection.current.childNodes.forEach(node => {
-      if (node instanceof HTMLElement){
-        node.style.opacity = "" + (1-scrollY/viewHeight)
-        node.style.scale = "" + (1+scrollY/viewHeight)
-      }
-    })
-  }, [scrollY])
-  return (
-    <section ref={heroSection} className="h-[200vh]">
-      <div className="slider">
-        <div className="min-w-0 mx-auto">
-          <h3 className="text-(--secondary-color)">Hi!</h3>
-          <h2>I'm</h2>
-          <h1>
-            <span className="text-(--primary-color) font-bold">Moeez</span>
-            <span>Raza</span>
-          </h1>
-          <hr />
-          <h3>
-            <span className="text-(--primary-color)">Full-Stack Web</span> & <span className="text-(--secondary-color)">Mobile App Developer</span>
-          </h3>
-        </div>
-        <img src="/images/background-left.png" className="img-left"/>
-        <img src="/images/background-right.png" className="img-right"/>
-      </div>
-    </section>
-  )
-}
 function Home(){
   return (
     <>
-      <HeroSection/>
+      <SingleColumnContent name="home">
+        <h3 className="text-(--secondary-color)">Hi!</h3>
+        <h2>I'm</h2>
+        <h1 className="text-wrap">
+          <span className="text-(--primary-color) font-bold">Moeez</span> <span>Raza</span>
+        </h1>
+        <hr />
+        <h3>
+          <span className="text-(--primary-color)">Full-Stack Web</span> & <span className="text-(--secondary-color)">Mobile App Developer</span>
+        </h3>
+      </SingleColumnContent>
       <section>
         <DoubleSidedContent title="About Me" img="/favicon.svg">
           <p className="text-justify">
